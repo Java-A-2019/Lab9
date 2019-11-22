e1c5d02cc265cb1825ade68f15a1c436c91e7b4a
zhaoyangyingmu <1065348898@qq.com>
2019-11-22 22:13:40 +0800

    Part 1 finished

--- src/launchDemos/Demo1.java
+++ src/launchDemos/Demo1.java
@@ -1,6 +1,9 @@
+package launchDemos;
+
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -9,13 +12,18 @@ public class Demo1 extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        // 控件
-        Button btOK = new Button("OK");
-        setWidthHeigh(btOK, 100 , 100);
+        // 控件Label
+        Label label = new Label("Welcome to JavaFX");
+        label.setMaxHeight(100);
+        label.setMinHeight(100);
+        label.setMaxWidth(200);
+        label.setMinWidth(200);
+        label.setStyle("-fx-background-color: #fff;"); // 设置成白色
+        label.setAlignment(Pos.CENTER); // 设置字体居中对齐
 
         // 布局
         StackPane pane = new StackPane();
-        pane.getChildren().add(btOK);
+        pane.getChildren().add(label);
         pane.setStyle("-fx-background-color:#aaa;"); // 设置成灰色
 
         // 主界面显示
@@ -23,13 +31,6 @@ public class Demo1 extends Application {
         stage.setTitle("Demo1");
         stage.setScene(scene);
         stage.show();
-    }
-
-    private void setWidthHeigh(Button bt, int width, int height) {
-        bt.setMaxHeight(height);
-        bt.setMinHeight(height);
-        bt.setMaxWidth(width);
-        bt.setMinWidth(width);
     }
 
     public static void main(String[] args) {
---
+++ src/launchDemos/Demo2.java
@@ -0,0 +1,40 @@
+package launchDemos;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Demo2 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // 控件
+        Button btOK = new Button("OK");
+        setWidthHeight(btOK, 100 , 100);
+
+        // 布局
+        StackPane pane = new StackPane();
+        pane.getChildren().add(btOK);
+        pane.setStyle("-fx-background-color:#aaa;"); // 设置成灰色
+
+        // 主界面显示
+        Scene scene = new Scene(pane, 200, 250);
+        stage.setTitle("Demo2");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void setWidthHeight(Region region, int width, int height) {
+        region.setMaxHeight(height);
+        region.setMinHeight(height);
+        region.setMaxWidth(width);
+        region.setMinWidth(width);
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
